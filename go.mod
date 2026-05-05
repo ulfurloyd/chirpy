cc09bbebc025c41020ef0aab86d0c647b67d2cc3
ulfurloyd <eklavyasood@gmail.com>
2026-05-06 00:07:41 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ulfurloyd/chirpy.git
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"sync/atomic"
+	"fmt"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	currentHits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", currentHits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("."))
+
+	var apiCfg apiConfig
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:       ":8082",
+		Handler:    mux,
+	}
+	srv.ListenAndServe()
+}
